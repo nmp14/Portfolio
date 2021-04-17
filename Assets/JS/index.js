806d97570e0ca2b98a48ed31964bf40fdeb6aa61
Nathan Peek <nmp14fsu@gmail.com>
2021-04-17 09:32:31 -0400

    Creat functions for opening and closing navBar

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -2,10 +2,14 @@ var cube = document.querySelector('.cube');
 var radioGroup = document.querySelector('.radio-group');
 var currentClass = 'show-front';
 const downArrow = document.getElementById("downArrow");
+const navItems = document.querySelectorAll("nav");
+const port = document.getElementById("port");
+const navPage = document.getElementById("navPage");
+const navCloseBtns = document.querySelectorAll(".nav-close");
 
 function changeSide() {
-    var checkedRadio = radioGroup.querySelector(':checked');
-    var showClass = 'show-' + checkedRadio.value;
+    if (radioGroup) var checkedRadio = radioGroup.querySelector(':checked');
+    const showClass = radioGroup ? 'show-' + checkedRadio.value : "show-front"
     if (currentClass) {
         cube.classList.remove(currentClass);
     }
@@ -24,7 +28,38 @@ const goToPortfolio = (e) => {
     const showClass = "show-bottom";
     cube.classList.add(showClass);
     currentClass = "show-bottom"
+
+    if (!navPage.classList.contains("hidden")) {
+        navPage.classList.add("hidden");
+    }
 }
 
-radioGroup.addEventListener('change', changeSide);
+// Open navbar menu
+const openNavBar = (e) => {
+    e.preventDefault()
+    e.stopPropagation();
+
+    navPage.classList.remove("hidden");
+    navPage.classList.add("slideIn");
+}
+// Close navbar menu
+const closeNavBar = (e) => {
+    e.preventDefault();
+
+    navPage.classList.add("slideOut");
+    navPage.classList.remove("slideIn");
+    setTimeout(() => {
+        navPage.classList.add("hidden");
+        navPage.classList.remove("slideOut");
+    }, 700)
+}
+
+if (radioGroup) radioGroup.addEventListener('change', changeSide);
 if (downArrow) downArrow.addEventListener("click", goToPortfolio);
+if (navItems) {
+    navItems.forEach(navItem => navItem.addEventListener("click", openNavBar));
+}
+if (port) port.addEventListener("click", goToPortfolio);
+if (navCloseBtns) {
+    navCloseBtns.forEach(btn => btn.addEventListener("click", closeNavBar));
+}
